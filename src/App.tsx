9e6e09dfbec8ec23604921977676d3e25e5e5022
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-06 19:18:26 -0300

    Prepara setup para trabalhar na construção da plataforma

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {gql} from "@apollo/client"
+import {gql, useQuery} from "@apollo/client"
 import { useEffect } from "react"
 import { client } from "./lib/apollo"
 
@@ -10,18 +10,22 @@ const GET_LESSONS_QUERY = gql`
     }
   }
 `
-function App() {
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
 
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+  console.log(data)
 
   return (
-   <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+  <ul>
+    {data?.lessons.map((lesson: Lesson) => {
+      return <li key={lesson.id}>{lesson.title}</li>
+    })}
+  </ul>
   )
 }
 
