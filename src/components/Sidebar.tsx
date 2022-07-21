4152a407234035f786845e99c9870b7ded41854c
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-20 21:33:06 -0300

    Implementa conteúdo na barra lateral

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,21 @@
+import { Lesson } from "../components/Lesson"
+
 export function Sidebar() {
     return (
     <aside className="w-[348px] bg-gray-700 p-6 border-L border-gray-600">
         <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
             Cronograma de Aulas
         </span>
+
+        <div className="flex flex-col gap-8">
+            <Lesson />
+            <Lesson />
+            <Lesson />
+            <Lesson />
+            <Lesson />
+            <Lesson />
+        </div>
+
     </aside>
     )
 }
