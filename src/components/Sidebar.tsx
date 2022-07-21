e7044347926850b313008c6e75749ef39049410d
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-20 21:13:33 -0300

    Implementa barra lateral

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,9 @@
 export function Sidebar() {
     return (
-    <h1>Sidebar</h1>
+    <aside className="w-[348px] bg-gray-700 p-6 border-L border-gray-600">
+        <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+            Cronograma de Aulas
+        </span>
+    </aside>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
-      
       colors: {
         green: {
           300: '#00B37E',
