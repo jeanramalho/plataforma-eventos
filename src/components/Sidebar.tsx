235ef012f228dd2443193bfe7860330cacf33fb7
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-20 22:34:45 -0300

    Termina estrutura barra lateral

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client"
 import { Lesson } from "../components/Lesson"
 
+const GET_LESSONS_QUERY = gql`
+query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      slug
+      title
+    }
+  }
+     `
+
+interface getLessonsQueryResponse {
+    lessons: {
+        id: string
+        title: string
+        slug: string
+        availableAt: string
+        lessonType: 'live' | 'class'
+    }[]
+}
+
 export function Sidebar() {
+    const { data } = useQuery<getLessonsQueryResponse>(GET_LESSONS_QUERY)
+
     return (
     <aside className="w-[348px] bg-gray-700 p-6 border-L border-gray-600">
         <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +33,17 @@ export function Sidebar() {
         </span>
 
         <div className="flex flex-col gap-8">
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
-            <Lesson />
+            {data?.lessons.map(lesson => {
+              return (
+                <Lesson 
+                key={lesson.id}
+               title={lesson.title}
+               slug={lesson.slug}
+               availableAt={new Date(lesson.availableAt)}
+               type= {lesson.lessonType}
+               /> )
+            })}
+            
         </div>
 
     </aside>
